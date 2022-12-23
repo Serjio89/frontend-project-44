9513ce2b31d281a6520c72242195255b13fe7594
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2022-12-23 11:13:08 +0300

    step 5 modify

--- src/cli.js
+++ src/cli.js
@@ -1,32 +1,8 @@
 import readlineSync from 'readline-sync';
-import { car, cdr } from '@hexlet/pairs/index.js';
 
-const numberOfCorrectAnswer = 3;
-
-export const greetings = () => {
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name?');
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}!`);
-  return username;
+  console.log(`Hello, ${userName}!`);
 };
-
-const startGame = (description, gameTask, name = '') => {
-  const username = name === '' ? greetings() : name;
-  console.log(description);
-  for (let i = 0; i < numberOfCorrectAnswer; i += 1) {
-    const task = gameTask();
-    const question = car(task);
-    const correctAnswer = cdr(task);
-    console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer !== correctAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${username}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${username}!`);
-};
-
-export default startGame;
+export default greeting;
