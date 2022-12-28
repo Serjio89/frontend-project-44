435791b6f1caa3d5fe5d34040fbfcf2d290680b6
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2022-12-28 12:35:46 +0300

    step 7 done

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -3,3 +3,22 @@ import beginGame from '../index.js';
 import getRandomNumber from '../randomNumber.js';
 
 const taskGame = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+const startGame = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const task = `${num1} ${num2}`;
+  const rightAnswer = gcd(num1, num2);
+  return [task, String(rightAnswer)];
+};
+
+export default () => {
+  beginGame(taskGame, startGame);
+};
