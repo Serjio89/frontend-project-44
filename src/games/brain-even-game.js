0bc83457f1da618c2e4e186110257975e0c164be
Serjio89 <115979687+Serjio89@users.noreply.github.com>
2022-12-29 18:22:03 +0300

    Update brain-even-game.js

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -2,21 +2,12 @@ import beginGame from '../index.js';
 
 import getRandomNumber from '../randomNumber.js';
 
-const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-const isPrime = (num) => {
-  for (let i = 2, j = Math.sqrt(num); i <= j; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return num > 1;
-};
-
+const isEven = (number) => number % 2 === 0;
+const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const startGame = () => {
-  const questionNum = getRandomNumber();
-  const rightAnswer = isPrime(questionNum) ? 'yes' : 'no';
-  return [questionNum, rightAnswer];
+  const number = getRandomNumber();
+  const answer = isEven(number) ? 'yes' : 'no';
+  return [number, answer];
 };
 
 export default () => {
