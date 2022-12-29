3525a3306254cb0a2ea94ae18828b566ee53b534
Serjio89 <115979687+Serjio89@users.noreply.github.com>
2022-12-29 18:22:58 +0300

    Update brain-prime-game.js

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -4,18 +4,18 @@ import getRandomNumber from '../randomNumber.js';
 
 const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function prime(num) {
+const isPrime = (num) => {
   for (let i = 2, j = Math.sqrt(num); i <= j; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
   return num > 1;
-}
+};
 
 const startGame = () => {
   const questionNum = getRandomNumber();
-  const rightAnswer = prime(questionNum) ? 'yes' : 'no';
+  const rightAnswer = isPrime(questionNum) ? 'yes' : 'no';
   return [questionNum, rightAnswer];
 };
 
